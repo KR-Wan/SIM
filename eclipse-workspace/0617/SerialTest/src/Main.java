0e0c9ed493877bfe08021c3cf150c74b4d494f98
KRW <tkdjzpa@naver.com>
2020-06-17 14:53:57 +0900

    0617

---
+++ eclipse-workspace/0617/SerialTest/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            (new Serial()).connect("COM8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
